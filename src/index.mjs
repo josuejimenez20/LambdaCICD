3c28e00c89e07264875e8d484a31725b9c0f6e49
Josue Jimenez <josuejimenez20@gmail.com>
2024-08-03 16:27:44 -0400

    Github Actions Lambda Function CI/CD V6

--- src/index.mjs
+++ src/index.mjs
@@ -2,7 +2,7 @@ export const handler = async (event) => {
     // TODO implement
     const response = {
       statusCode: 200,
-      body: JSON.stringify('Hello from Lambda! - Updated V5'),
+      body: JSON.stringify('Hello from Lambda! - Updated V6'),
     };
     return response;
   };
